0398978a36ea55cb02e52a467e42b619944e3550
Cezary Walczak <cezary.walczak.pl@gmail.com>
2018-06-21 14:34:13 +0200

    newmessage component

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,17 +1,32 @@
 <template lang="pug">
   section#chat
-    h2 Chat {{name}}
+    h2 Chat
+    ul
+      li
+        span name
+        span message
+        span time
+    NewMessage(:name="name")
 </template>
 
 <script>
+import NewMessage from '@/components/NewMessage'
+
 export default {
   name: 'Chat',
   props: {
     name: String
+  },
+  components: {
+    NewMessage
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-
+  section#chat
+    ul
+      li
+        span
+          display block
 </style>
---
+++ src/components/NewMessage.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+  section#new-message
+    form(@submit.prevent="addMessage")
+      label(for="new-message") New Message (enter to add)
+      input(type="text" name="new-message" v-model="newMessage")
+</template>
+
+<script>
+export default {
+  name: 'NewMessage',
+  props: {
+    name: String
+  },
+  data() {
+    return {
+      newMessage: null
+    }
+  },
+  methods: {
+    addMessage() {
+      console.log(this.newMessage, this.name, Date.now())
+    }
+  }
+}
+</script>
+
