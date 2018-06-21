7675ba635eabec65f60aa4254aaf2257d95e030a
Cezary Walczak <cezary.walczak.pl@gmail.com>
2018-06-21 15:24:17 +0200

    realtime events

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -2,15 +2,16 @@
   section#chat
     h2 Chat
     ul
-      li
-        span name
-        span message
-        span time
+      li(v-for="message in messages")
+        span {{message.name}}
+        span {{message.content}}
+        span {{message.timestamp}}
     NewMessage(:name="name")
 </template>
 
 <script>
 import NewMessage from '@/components/NewMessage'
+import db from '@/firebase/init'
 
 export default {
   name: 'Chat',
@@ -18,7 +19,26 @@ export default {
     name: String
   },
   components: {
-    NewMessage
+    NewMessage,
+  },
+  data() {
+    return {
+      messages: []
+    }
+  },
+  created() {
+    db.collection('messages').orderBy('timestamp').onSnapshot(snapshot => {
+      snapshot.docChanges().forEach(change => {
+        if (change.type == 'added') {
+          this.messages.push({
+            id: change.doc.id,
+            name: change.doc.data().name,
+            content: change.doc.data().content,
+            timestamp: change.doc.data().timestamp
+          })
+        }
+      })
+    })
   }
 }
 </script>
